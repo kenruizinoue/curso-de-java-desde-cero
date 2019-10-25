02573a4cbe7add3c073fb7e6be5ea06795d73b76
worldken <ken.inoue07@gmail.com>
2019-10-25 16:20:57 -0700

    Archivo01Comentarios

---
+++ src/modulo2/Archivo01Comentarios.java
@@ -0,0 +1,16 @@
+package modulo2;
+
+public class Archivo01Comentarios {
+    public static void main(String[] args) {
+//      este es un comentario
+        // puedo escribir lo que quiera
+        System.out.println("Hola");
+
+        // mult. lineas
+        /*
+        linea 1
+        linea 2
+        linea 3
+        * */
+    }
+}
