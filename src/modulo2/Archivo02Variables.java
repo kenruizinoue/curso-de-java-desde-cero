bd19ab38a40203fdf9f9e4a33af2ba912cd38f3d
worldken <ken.inoue07@gmail.com>
2019-10-25 17:28:38 -0700

    Archivo02Variables

---
+++ src/modulo2/Archivo02Variables.java
@@ -0,0 +1,16 @@
+package modulo2;
+
+public class Archivo02Variables {
+    public static void main(String[] args) {
+        // tipo nombre = valor
+        int naranja = 2;
+        int precioNaranja = 5;
+
+        System.out.print("El precio de la naranja es de: $");
+        System.out.println(precioNaranja);
+
+        precioNaranja = 15;
+
+        System.out.println("El precio de la naranja es de: $" + precioNaranja);
+    }
+}
