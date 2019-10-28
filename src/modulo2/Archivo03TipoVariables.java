44b2e5b63e01e411d6cca43537526d1611848e8a
worldken <ken.inoue07@gmail.com>
2019-10-27 22:35:40 -0700

    Archivo03TipoVariables

---
+++ src/modulo2/Archivo03TipoVariables.java
@@ -0,0 +1,19 @@
+package modulo2;
+
+public class Archivo03TipoVariables {
+    public static void main(String[] args) {
+        int precioNaranja = 5;
+        System.out.println("Precio de la naranja: " + precioNaranja);
+
+        double precioManzana = 6.25;
+        System.out.println("Precio de la manzana: " + precioManzana);
+
+        String nombre = "Juanito Perez";
+        System.out.println("Hola " + nombre);
+
+        String precioUvas = "$5.5";
+        double precioUvas2 = 5.5;
+        System.out.println("Precio de las uvas: " + precioUvas);
+        System.out.println("Precio de las uvas: " + precioUvas2);
+    }
+}
