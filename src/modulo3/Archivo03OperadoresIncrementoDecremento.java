bbd72e3531aa5e4abb1d2d68726398768734622a
worldken <ken.inoue07@gmail.com>
2019-11-21 15:18:35 -0800

    Archivo03OperadoresIncrementoDecremento

---
+++ src/modulo3/Archivo03OperadoresIncrementoDecremento.java
@@ -0,0 +1,16 @@
+package modulo3;
+
+public class Archivo03OperadoresIncrementoDecremento {
+    public static void main(String[] args) {
+        double contador = 1;
+        System.out.println("Contenido contador: " + contador);
+
+        contador++;
+        contador++;
+        System.out.println("Contenido contador: " + contador);
+
+        contador--;
+        System.out.println("Contenido contador: " + contador);
+
+    }
+}
