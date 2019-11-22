27b51470b742c2dffb1963818141c111f0e8ce63
worldken <ken.inoue07@gmail.com>
2019-11-21 19:12:46 -0800

    Archivo04CondicionalIf

---
+++ src/modulo3/Archivo04CondicionalIf.java
@@ -0,0 +1,15 @@
+package modulo3;
+
+public class Archivo04CondicionalIf {
+    public static void main(String[] args) {
+        // Booleano true
+        boolean tengoDescuento = false;
+        int numero = 1;
+
+        if (tengoDescuento) {
+            System.out.println("Tienes descuento! :D");
+        } else {
+            System.out.println("No tienes descuento :(");
+        }
+    }
+}
